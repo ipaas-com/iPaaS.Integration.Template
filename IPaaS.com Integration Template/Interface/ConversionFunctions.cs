fa59c50a80d92550338f62996562b8ea7453689a
Vincent Valentine <marjak101@gmail.com>
2021-10-25 09:19:22 -0700

    Init Template

---
+++ IPaaS.com Integration Template/Interface/ConversionFunctions.cs
@@ -0,0 +1,33 @@
+﻿using Integration.Data.IPaaSApi;
+using Integration.Data.IPaaSApi.Model;
+using Integration.Data.Utilities;
+using Integration.DataModels;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Integration.Constants;
+
+namespace Integration.Data.Interface
+{
+    public class ConversionFunctions : Integration.Abstract.ConversionFunctions 
+    {
+        private static Connection ContextConnection { get { return (Connection)Integration.Abstract.ConversionFunctions.ContextConnection; } }
+        private static Settings IntegrationSettings { get { return (Settings)ContextConnection.Settings; } }
+        private static CallWrapper IntegrationCallWrapper { get { return (CallWrapper)ContextConnection.CallWrapper; } }
+
+        [ThreadStatic]
+        private static IPaaSApiCallWrapper _iPaaSApiCallWrapper;
+        public static IPaaSApiCallWrapper iPaaSApiCallWrapper
+        {
+            get
+            {
+                if (_iPaaSApiCallWrapper == null)
+                    _iPaaSApiCallWrapper = new IPaaSApiCallWrapper();
+                return _iPaaSApiCallWrapper;
+            }
+        }
+    }
+}
---
+++ IPaaS.com Integration Template/Interface/Settings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Integration.Data.Interface
+{
+    public class Settings : Integration.Abstract.Settings
+    {
+        public string Url { get { return this.GetSetting("API Url", required: true); } }
+        public string APIUser { get { return this.GetSetting("API User", required: true); } }
+        public string APIPassword { get { return this.GetSetting("API Password", required: true); } }
+    }
+}
