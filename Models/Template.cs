7e9bfc39dbc0bb9e827d6655c45050a6210b766a
Vance Berisford <vanceho@hotmail.com>
2025-07-21 16:46:06 -0400

    Add files via upload
    Remove string.format, add NotFoundHandling

--- Models/Template.cs
+++ Models/Template.cs
@@ -71,6 +71,10 @@ namespace Integration.DataModels
             throw new NotImplementedException();
         }
 
+        public override async Task<List<BulkTransferRequest>> Poll(CallWrapper activeCallWrapper, string filter)
+        {
+            throw new NotImplementedException();
+        }
         #endregion
 
         #region CustomMethods
